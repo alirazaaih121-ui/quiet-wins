e68ec3f65f624a1e1b0a2f516c8a3de18fb33a6e
abu-huraira-abid <huraira70140712@gmail.com>
2025-11-10 11:52:13 -0800

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Quiet-Book/', // <--- add this line
+  base: '/Quiet-Wins/', // <--- add this line
 });
